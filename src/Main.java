c5f62dbdf84c3b782c49084e39dd42c542e3360f
Brian W Wiatrek <brianwiatrek@aol.com>
2024-08-31 19:06:28 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+/*
+Short Description:  This program will create three dates using the MyDate class and print out the date to the console.
+Author:  Brian Wiatrek
+Date:  August 25, 2024
+*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        long elapsedTime = 561555550000L;
+        MyDate testDate = new MyDate(elapsedTime);
+        System.out.printf("Year: %d Month: %d Day: %d\n", testDate.getYear(), testDate.getMonth(), testDate.getDay());
+
+        MyDate testDate2 = new MyDate();
+        System.out.printf("Year: %d Month: %d Day: %d\n", testDate2.getYear(), testDate2.getMonth(), testDate2.getDay());
+
+        elapsedTime = 34355555133101L;
+        MyDate testDate3 = new MyDate(elapsedTime);
+        System.out.printf("Year: %d Month: %d Day: %d\n", testDate3.getYear(), testDate3.getMonth(), testDate3.getDay());
     }
 }
--- src/MyDate.java
+++ src/MyDate.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+/*
+Short Description:  This class will create a date.  The date can be defaulted to the current date (no-arg constructor),
+                    it can be defaulted to three integers passed in representing the year, month, and day, or it can
+                    be created by passing in the elapsed milliseconds from January 1, 1970.
+Author:  Brian Wiatrek
+Date:  August 25, 2024
+*/
+import java.time.LocalDateTime;
 
 public class MyDate {
+    int year;
+    int month;
+    int day;
+
+    public MyDate() {
+        this.year = LocalDateTime.now().getYear();
+        this.month = LocalDateTime.now().getMonthValue() - 1;
+        this.day = LocalDateTime.now().getDayOfMonth();
+    }
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public MyDate(long elapsedTime){
+        LocalDateTime tempDateTime = LocalDateTime.of(1970, 1, 1, 0,0,0,0).plusSeconds(elapsedTime/1000);
+        this.month = tempDateTime.getMonthValue() - 1;
+        this.day = tempDateTime.getDayOfMonth();
+        this.year = tempDateTime.getYear();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDate(long elapsedTime){
+        LocalDateTime tempDateTime = LocalDateTime.of(1970, 1, 1, 0,0,0,0).plusSeconds(elapsedTime/1000);
+        this.month = tempDateTime.getMonthValue() - 1;
+        this.day = tempDateTime.getDayOfMonth();
+        this.year = tempDateTime.getYear();
+    }
 }
